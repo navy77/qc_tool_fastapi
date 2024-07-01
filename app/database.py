f45aab869e1f6976ffd97facdf3c8b393b6f85ce
arthit.s <arthit.sekmoh@gmail.com>
2024-07-01 09:13:41 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,10 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+import urllib.parse
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+server = os.getenv("SERVER")
+database = os.getenv("DATABASE")
+user_login = os.getenv("USER_LOGIN")
+password = os.getenv("PASSWORD")
+
+encoded_password = urllib.parse.quote_plus(password)
+
+DATABASE_URL = f'mssql+pymssql://{user_login}:{encoded_password}@{server}/{database}'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
